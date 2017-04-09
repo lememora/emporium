143f1246dae78813f6533cbc703ea3c5802e1b31
Rafael Moraes <contact@moraesrafael.com>
2017-04-09 19:12:52 -0400

    testing a branch

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     root 'uploads#new'
   end
 
-  root 'uploads#new'
+  root 'website#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
